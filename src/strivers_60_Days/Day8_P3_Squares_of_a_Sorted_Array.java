2bafee91185c5dd4992356a5b28f478ade58a5d7
Sunil L M <sunillm2026@gmail.com>
2026-06-10 17:12:29 +0530

    Solved:Squares of a Sorted Array

---
+++ src/strivers_60_Days/Day8_P3_Squares_of_a_Sorted_Array.java
@@ -0,0 +1,33 @@
+package strivers_60_Days;
+
+public class Day8_P3_Squares_of_a_Sorted_Array {
+
+	public static void main(String[] args) {
+		int[]arr= {-4, -2, -1, 0, 3, 5, 7};
+		int[]ans=sortedSquare(arr);
+		for(int i=0;i<ans.length;i++) {
+			System.out.print(ans[i]+" ");
+		}
+	}
+	static int[] sortedSquare(int[]arr) {
+		int k=arr.length-1;
+		int[]result=new int[arr.length];
+		int i=0;
+		int j=arr.length-1;
+		while(i<=j) {
+			int rightSquare=arr[i]*arr[i];
+			int leftSquare=arr[j]*arr[j];
+			if(rightSquare>leftSquare) {
+				result[k]=rightSquare;
+				i++;
+			}
+			else {
+				result[k]=leftSquare;
+				j--;
+			}
+			k--;
+		}
+		return result;
+	}
+
+}
