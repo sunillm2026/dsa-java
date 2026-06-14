72e8da7c3ea6c97dc5203181c4dff3d2c96bd984
Sunil L M <sunillm2026@gmail.com>
2026-06-14 16:50:59 +0530

    Solved:Two Sum using Brute Force Method

---
+++ src/strivers_60_Days/Day10_P4_Two_Sum_Brute_Force.java
@@ -0,0 +1,23 @@
+package strivers_60_Days;
+
+public class Day10_P4_Two_Sum_Brute_Force {
+
+	public static void main(String[] args) {
+		int[]arr= {2,6,5,8,11};
+		int target= 14;
+		twoSum(arr,target);
+		
+	}
+		static void twoSum(int[]arr,int target) {
+      		for(int i=0;i<arr.length;i++) {
+	      		int sum=0;
+		  	
+			    for(int j=i+1;j<arr.length;j++) {
+				   sum=arr[i]+arr[j];
+				   if(sum==target) {
+					   System.out.println(i+" "+j);
+				}
+			}
+		}
+	}
+}
