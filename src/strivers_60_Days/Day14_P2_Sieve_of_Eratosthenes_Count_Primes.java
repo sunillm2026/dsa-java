a17e06c8da700e0359141d1ee893fab7ea652d4f
Sunil L M <sunillm2026@gmail.com>
2026-06-29 19:40:33 +0530

    Solved Count primes using Sieve of Eratosthenes

---
+++ src/strivers_60_Days/Day14_P2_Sieve_of_Eratosthenes_Count_Primes.java
@@ -0,0 +1,41 @@
+package strivers_60_Days;
+import java.util.Scanner;
+public class Day14_P2_Sieve_of_Eratosthenes_Count_Primes {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int ans=countPrime(n);
+		System.out.println(ans);
+	}
+		
+		static int countPrime(int n) {
+			if(n <= 2) {
+				return 0;
+			}
+		
+		// put true to all the elements
+		boolean[]arr=new boolean[n];
+		for(int i=2;i<n;i++) {
+			arr[i]=true;
+		}
+		
+		// put false for multiples of prime
+		for(int j=2;j<n;j++) {
+			if(arr[j]) {
+				for(int k=j*j;k<n;k+=j) {
+					arr[k]=false;
+				}
+			}
+		}
+		
+		// count the number of primes
+		int count=0;
+		for(int j=2;j<n;j++) {
+			if(arr[j]) {
+				count++;
+			}
+		}
+		return count;
+	}
+}
